5aac4e2a428c8aa4cc72e38df9f1c1e968b89239
IrisRizos <92450816+IrisRizos@users.noreply.github.com>
2022-06-26 20:31:24 +0200

    Create PR2_blast.sh

---
+++ Homogenisation_Workflow/PR2_blast.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+#SBATCH --job-name blast
+#SBATCH --cpus-per-task=6 
+#SBATCH -o o.Barc_blast
+#SBATCH -e e.Barc_blast
+
+blastn -query Dataset.fasta -db PR2_18S -out Dataset_080221.blast.out -evalue 0.01 -outfmt 6 -max_target_seqs 15
+
+# Run on 08.02.2021
