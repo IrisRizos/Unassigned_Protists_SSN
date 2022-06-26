986f8a1cebf9ba40a19cce7340656769bfcbc0de
IrisRizos <92450816+IrisRizos@users.noreply.github.com>
2022-06-26 20:30:28 +0200

    Create PR2_mkdb.sh

---
+++ Homogenisation_Workflow/PR2_mkdb.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+#SBATCH --job-name mkdb
+#SBATCH --cpus-per-task=4 
+#SBATCH -o o.mkdb
+#SBATCH -e e.mkdb
+
+module load blast/
+
+makeblastdb -in /shared/projects/formaldark/PR2_blast/pr2_version_4.12.0_18S_taxo_long_nostrain.fasta -dbtype nucl -out PR2_18S
+
+# Run on 03.02.2021
